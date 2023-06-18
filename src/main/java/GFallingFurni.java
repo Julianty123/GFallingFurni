c86f77e36e9f6d6c0143ea8be3e0167b05e51adb
Julian <julian.a.p.c@hotmail.com>
2023-06-18 03:34:14 -0500

    Fixing & organizing code

--- src/main/java/GFallingFurni.java
+++ src/main/java/GFallingFurni.java
@@ -5,18 +5,19 @@ import gearth.extensions.parsers.HEntityUpdate;
 import gearth.extensions.parsers.HPoint;
 import gearth.protocol.HMessage;
 import gearth.protocol.HPacket;
+
 import javafx.application.Platform;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.logging.LogManager;
 
@@ -24,27 +25,25 @@ import java.util.logging.LogManager;
 @ExtensionInfo(
         Title = "GFallingFurni",
         Description = "Classic extension, enjoy it!",
-        Version = "1.2.3",
+        Version = "1.2.4",
         Author = "Julianty"
 )
 
 public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
-    public LinkedList<String> arrayList = new LinkedList<>(); // LinkedList is better than ArrayList
-    public LinkedList<Integer> poisonFurniList = new LinkedList<>();
-    public LinkedList<Integer> specificFurniList = new LinkedList<>();
+    public ArrayList<Integer> listPoisonFurniture = new ArrayList<>();
+    public ArrayList<Integer> listSpecificFurniture = new ArrayList<>();
+    public ArrayList<HPoint> listEqualsCoords = new ArrayList<>();
 
-    public CheckBox checkPoison, checkAutodisable, checkSpecificPoint, checkSpecificFurni;
-    public RadioButton radioEqualsCoords, radioCurrent, radioSpecificPoint;
+    public ToggleGroup Mode;
     public TextField fieldDelay;
     public Button buttonStart, buttonDeleteSpecific;
+    public CheckBox checkPoison, checkAutodisable, checkSpecificPoint, checkSpecificFurni;
+    public RadioButton radioEqualsCoords, radioCurrent, radioSpecificPoint, radioCoordFurni, radioWalk;
 
-    public String YourName;
-    public int YourIndex = -1;
-    public int FurniID, UserID, newXCoordFurni, newYCoordFurni, xSpecificPoint, ySpecificPoint;
-    public ArrayList<HPoint> listEqualsCoords = new ArrayList<>();
     public HPoint walkTo;
-    public ToggleGroup Mode;
-    public RadioButton radioCoordFurni, radioWalk;
+    public String yourName;
+    public int yourIndex = -1;
+    public int userId, newXCoordFurni, newYCoordFurni, xSpecificPoint, ySpecificPoint;
 
     @Override
     public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {}
@@ -52,25 +51,21 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
     @Override
     public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
         if(NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()).equals("Ctrl")){
-            Platform.runLater(() -> {
-                buttonStart.setText("---ON---"); buttonStart.setTextFill(Color.GREEN);
-            });
+            Platform.runLater(() -> { buttonStart.setText("---ON---"); buttonStart.setTextFill(Color.GREEN); });
         }
     }
 
     @Override
     public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
         if(NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()).equals("Ctrl")){
-            Platform.runLater(() -> turnOffButton());
+            Platform.runLater(this::turnOffButton);
         }
     }
 
     @Override
     protected void onShow() {
-        // The packet is sent to the server and a response is obtained from the CLIENT !!
-        sendToServer(new HPacket("InfoRetrieve", HMessage.Direction.TOSERVER));
-        // With this it's not necessary to restart the room
-        sendToServer(new HPacket("AvatarExpression", HMessage.Direction.TOSERVER, 0));
+        sendToServer(new HPacket("{out:InfoRetrieve}"));    // When sent to the server, the client responds!
+        sendToServer(new HPacket("{out:AvatarExpression}{i:0}"));   // With this it's not necessary to restart the room
 
         LogManager.getLogManager().reset();
         try {
@@ -91,9 +86,9 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
     @Override
     protected void onHide() {   // Runs this when the GUI is closed
         Platform.runLater(this::turnOffButton); // Platform.exit();
-        arrayList.clear(); poisonFurniList.clear();
+        listSpecificFurniture.clear(); listPoisonFurniture.clear();
         radioCurrent.setSelected(true);
-        YourIndex = -1;
+        yourIndex = -1;
 
         try {
             GlobalScreen.unregisterNativeHook();
@@ -141,15 +136,15 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
         // Intercepts the client's response and does something ...
         intercept(HMessage.Direction.TOCLIENT, "UserObject", hMessage -> {
             // Be careful, the data must be obtained in the order of the packet
-            UserID = hMessage.getPacket().readInteger();
-            YourName = hMessage.getPacket().readString();
+            userId = hMessage.getPacket().readInteger();
+            yourName = hMessage.getPacket().readString();
         });
 
         // Response of packet AvatarExpression (gets userIndex)
         intercept(HMessage.Direction.TOCLIENT, "Expression", hMessage -> {
             // First integer is index in room, second is animation id, i think
-            if(primaryStage.isShowing() && YourIndex == -1){ // this could avoid any bug
-                YourIndex = hMessage.getPacket().readInteger();
+            if(primaryStage.isShowing() && yourIndex == -1){ // this could avoid any bug
+                yourIndex = hMessage.getPacket().readInteger();
             }
         });
 
@@ -157,14 +152,9 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
         intercept(HMessage.Direction.TOSERVER, "UseFurniture", this::methodOneorDoubleClick);
 
         intercept(HMessage.Direction.TOSERVER, "MoveAvatar", hMessage -> {
-            /*if(checkEqualsCoords.isSelected()){
-                xEqualsCoord = hMessage.getPacket().readInteger();  yEqualsCoord = hMessage.getPacket().readInteger();
-
-            }
-            else */
             if(checkSpecificPoint.isSelected()){
                 xSpecificPoint = hMessage.getPacket().readInteger();    ySpecificPoint = hMessage.getPacket().readInteger();
-                Platform.runLater(() -> checkSpecificPoint.setText("(" + xSpecificPoint + ", " + ySpecificPoint + ")")); // Platform.exit();
+                Platform.runLater(() -> checkSpecificPoint.setText("(" + xSpecificPoint + ", " + ySpecificPoint + ")"));
                 hMessage.setBlocked(true);
                 checkSpecificPoint.setSelected(false);
             }
@@ -180,14 +170,14 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
             }
         });
 
-        // Intercept this packet when you enter or restart a room
+        // Intercept this packet when any user enters the room
         intercept(HMessage.Direction.TOCLIENT, "Users", hMessage -> {
             try {
                 HPacket hPacket = hMessage.getPacket();
                 HEntity[] roomUsersList = HEntity.parse(hPacket);
                 for (HEntity hEntity: roomUsersList){
-                    if(hEntity.getName().equals(YourName)){    // In another room, the index changes
-                        YourIndex = hEntity.getIndex();
+                    if(hEntity.getName().equals(yourName)){    // In another room, the index changes
+                        yourIndex = hEntity.getIndex();
                     }
                 }
             } catch (Exception ignored) { }
@@ -199,17 +189,15 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
             for (HEntityUpdate hEntityUpdate: HEntityUpdate.parse(hPacket)){
                 try {
                     int CurrentIndex = hEntityUpdate.getIndex();
-                    if(YourIndex == CurrentIndex){
+                    if(yourIndex == CurrentIndex){
                         if(checkAutodisable.isSelected()){
                             HPoint currentHPoint = new HPoint(hEntityUpdate.getMovingTo().getX(), hEntityUpdate.getMovingTo().getY());
 
-                            for(HPoint equalsCoords: listEqualsCoords){
-                                if((newXCoordFurni == currentHPoint.getX() && newYCoordFurni == currentHPoint.getY()) ||
-                                        (equalsCoords.getX() == currentHPoint.getX() && equalsCoords.getY() == currentHPoint.getY()) ||
-                                        (xSpecificPoint == currentHPoint.getX() && ySpecificPoint == currentHPoint.getY())){
-                                    Platform.runLater(this::turnOffButton);
-                                    break;
-                                }
+                            if((newXCoordFurni == currentHPoint.getX() && newYCoordFurni == currentHPoint.getY()) ||
+                                    (walkTo.getX() == currentHPoint.getX() && walkTo.getY() == currentHPoint.getY()) ||
+                                    (xSpecificPoint == currentHPoint.getX() && ySpecificPoint == currentHPoint.getY())){
+                                Platform.runLater(this::turnOffButton);
+                                break;
                             }
                         }
                     }
@@ -227,27 +215,12 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
         // Intercepts when a furni is moved from one place to another with wired!
         intercept(HMessage.Direction.TOCLIENT, "SlideObjectBundle", hMessage -> {
             if("---ON---".equals(buttonStart.getText())){
-                int oldX = hMessage.getPacket().readInteger();
-                int oldY = hMessage.getPacket().readInteger();
-                newXCoordFurni = hMessage.getPacket().readInteger();
-                newYCoordFurni = hMessage.getPacket().readInteger();
+                int oldX = hMessage.getPacket().readInteger();  int oldY = hMessage.getPacket().readInteger();
+                newXCoordFurni = hMessage.getPacket().readInteger();    newYCoordFurni = hMessage.getPacket().readInteger();
                 int NotUse = hMessage.getPacket().readInteger();
-                FurniID = hMessage.getPacket().readInteger();   // Moving furniture id
+                int furnitureId = hMessage.getPacket().readInteger();
 
-                // A thread is created, this is necessary to avoid "Lagging" when its used the Thread.Sleep
-                Thread t1 = new Thread(() -> {
-                    try {
-                        Thread.sleep(Integer.parseInt(fieldDelay.getText())); // The time that the thread will sleep
-                    }catch (InterruptedException ignored){}
-
-                    if(specificFurniList.size() > 0){
-                        if (specificFurniList.contains(FurniID)){ SitOnTheChair(); }
-                    }
-                    else { // When is equals to 0
-                        if (!poisonFurniList.contains(FurniID)){ SitOnTheChair(); }
-                    }
-                });
-                t1.start(); // Thread started
+                threadToSleep(furnitureId);
             }
         });
     }
@@ -255,59 +228,60 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
     private void methodOneorDoubleClick(HMessage hMessage) {
         int furniId = hMessage.getPacket().readInteger();
         if(checkSpecificFurni.isSelected()){
-            if(poisonFurniList.contains(furniId)){
-                String SaySomething = "You cant select this furni because its on the poison list!";
-                sendToClient(new HPacket("Whisper", HMessage.Direction.TOCLIENT, UserID, SaySomething, 0, 34, 0, -1, UserID));
-            }
-            if(!poisonFurniList.contains(furniId)){
-                if(!specificFurniList.contains(furniId)){
-                    specificFurniList.add(furniId);
-                    Platform.runLater(() -> checkSpecificFurni.setText("Specific Furnis (" + specificFurniList.size() + ")"));
+            if(!listPoisonFurniture.contains(furniId)){
+                if(!listSpecificFurniture.contains(furniId)){
+                    listSpecificFurniture.add(furniId);
+                    Platform.runLater(() -> checkSpecificFurni.setText("Specific Furnis (" + listSpecificFurniture.size() + ")"));
                     String SaySomething = "The furni has been added successfully";
-                    sendToClient(new HPacket("Whisper", HMessage.Direction.TOCLIENT, UserID, SaySomething, 0, 34, 0, -1, UserID));
+                    // Packet Structure: {in:Whisper}{i:1956}{s:"Whatever thing here"}{i:0}{i:34}{i:0}{i:-1}{i:1956}
+                    sendToClient(new HPacket("Whisper", HMessage.Direction.TOCLIENT, userId, SaySomething, 0, 34, 0, -1, userId));
                 }
+            }
+            else{
+                String SaySomething = "You cant select this furni because its on the poison list!";
+                sendToClient(new HPacket("Whisper", HMessage.Direction.TOCLIENT, userId, SaySomething, 0, 34, 0, -1, userId));
             }
         }
         if(checkPoison.isSelected()){
-            if(!specificFurniList.contains(furniId)){
-                if(!poisonFurniList.contains(furniId)){
-                    poisonFurniList.add(furniId);
-                    Platform.runLater(() -> checkPoison.setText("Poison Furnis (" + poisonFurniList.size() + ")"));
-                    // Packet Structure
-                    // {in:Whisper}{i:1956}{s:"Whatever thing here"}{i:0}{i:34}{i:0}{i:-1}{i:1956}
+            if(!listSpecificFurniture.contains(furniId)){
+                if(!listPoisonFurniture.contains(furniId)){
+                    listPoisonFurniture.add(furniId);
+                    Platform.runLater(() -> checkPoison.setText("Poison Furnis (" + listPoisonFurniture.size() + ")"));
                     String SaySomething = "The furni with ID "+ furniId +" has been added successfully";
-                    sendToClient(new HPacket("Whisper", HMessage.Direction.TOCLIENT, UserID, SaySomething, 0, 34, 0, -1, UserID));
+                    sendToClient(new HPacket("Whisper", HMessage.Direction.TOCLIENT, userId, SaySomething, 0, 34, 0, -1, userId));
                 }
             }
-            if(specificFurniList.contains(furniId)){
+            else{
                 String SaySomething = "You cant select this furni because its on the specific furni list!";
-                sendToClient(new HPacket("Whisper", HMessage.Direction.TOCLIENT, UserID, SaySomething, 0, 34, 0, -1, UserID));
+                sendToClient(new HPacket("Whisper", HMessage.Direction.TOCLIENT, userId, SaySomething, 0, 34, 0, -1, userId));
             }
         }
     }
 
     private void DoSomething(HMessage hMessage) {
         if("---ON---".equals(buttonStart.getText())){
-            FurniID = hMessage.getPacket().readInteger();
-            int withOutUse = hMessage.getPacket().readInteger();
-            newXCoordFurni = hMessage.getPacket().readInteger();
-            newYCoordFurni = hMessage.getPacket().readInteger();
+            int furnitureId = hMessage.getPacket().readInteger();   int withOutUse = hMessage.getPacket().readInteger();
+            newXCoordFurni = hMessage.getPacket().readInteger();    newYCoordFurni = hMessage.getPacket().readInteger();
 
-            // A thread is created, this is necessary to avoid "Lagging" when its used the Thread.Sleep
-            Thread t1 = new Thread(() -> {
-                try {
-                    Thread.sleep(Integer.parseInt(fieldDelay.getText())); // The time that the thread will sleep
-                }catch (InterruptedException ignored){}
-
-                if(specificFurniList.size() > 0){
-                    if (specificFurniList.contains(FurniID)){ SitOnTheChair(); }
-                }
-                else { // When is equals to 0
-                    if (!poisonFurniList.contains(FurniID)){ SitOnTheChair(); }
-                }
-            });
-            t1.start(); // Thread started
+            threadToSleep(furnitureId);
         }
+    }
+
+    private void threadToSleep(int furnitureId){
+        // A thread is created, this is necessary to avoid "Lagging" when its used the Thread.Sleep
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(Integer.parseInt(fieldDelay.getText())); // The time that the thread will sleep
+            }catch (InterruptedException ignored){}
+
+            if(listSpecificFurniture.size() > 0){
+                if (listSpecificFurniture.contains(furnitureId)){ SitOnTheChair(); }
+            }
+            else { // When is equals to 0
+                if (!listPoisonFurniture.contains(furnitureId)){ SitOnTheChair(); }
+            }
+        });
+        t1.start(); // Thread started
     }
 
     private void SitOnTheChair(){
@@ -328,12 +302,6 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
         else if(txtRadio.contains("Specific")){
             sendToServer(new HPacket("MoveAvatar", HMessage.Direction.TOSERVER, xSpecificPoint, ySpecificPoint));
         }
-
-        if(checkSpecificFurni.isSelected()){
-            if(specificFurniList.contains(FurniID)){
-                sendToServer(new HPacket("MoveAvatar", HMessage.Direction.TOSERVER, newXCoordFurni, newYCoordFurni));
-            }
-        }
     }
 
     public void handleButtonStart(){
@@ -350,12 +318,23 @@ public class GFallingFurni extends ExtensionForm implements NativeKeyListener {
     }
 
     public void handleErasePoisons() {
-        poisonFurniList.clear();
-        Platform.runLater(() -> checkPoison.setText("Poison Furnis (" + poisonFurniList.size() + ")"));
+        listPoisonFurniture.clear();
+        Platform.runLater(() -> checkPoison.setText("Poison Furnis (" + listPoisonFurniture.size() + ")"));
     }
 
     public void handleDeleteSpecific() {
-        specificFurniList.clear();
-        Platform.runLater(() -> checkSpecificFurni.setText("Specific Furnis (" + specificFurniList.size() + ")"));
+        listSpecificFurniture.clear();
+        Platform.runLater(() -> checkSpecificFurni.setText("Specific Furnis (" + listSpecificFurniture.size() + ")"));
     }
 }
+
+/*
+  Logic in table, for better understanding
+  Example:
+
+  | Coord Furni   | Walk To       |
+  |---------------|---------------|
+  | AnyTile       | Coord Furni   | Custom Mode
+  | SpecificTile  | SpecificTile  | Equals Mode - mix here
+  | AnyTile       | SpecificTile  | Specific Point Mode
+*/
