5d848ebbb940d865acd2456736e7ebafaf4dfc41
Julianty123 <julian.a.p.c@hotmail.com>
2023-10-22 15:52:21 -0500

    Dark Theme 😍😏

--- src/main/java/GFallingFurniLauncher.java
+++ src/main/java/GFallingFurniLauncher.java
@@ -1,5 +1,10 @@
 import gearth.extensions.ExtensionForm;
 import gearth.extensions.ExtensionFormCreator;
+import gearth.ui.themes.Theme;
+import gearth.ui.themes.ThemeFactory;
+import gearth.ui.titlebar.DefaultTitleBarConfig;
+import gearth.ui.titlebar.TitleBarController;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,14 +13,27 @@ import javafx.stage.Stage;
 public class GFallingFurniLauncher extends ExtensionFormCreator {
 
     @Override
-    public ExtensionForm createForm(Stage primaryStage) throws Exception {
+    public ExtensionForm createForm(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("GFallingFurni.fxml"));
         Parent root = loader.load();
 
-        primaryStage.setTitle("GFallingFurni");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
-        primaryStage.setAlwaysOnTop(true);
+        stage.setTitle("GFallingFurni");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.setAlwaysOnTop(true);
+
+        Theme defaultTheme = ThemeFactory.themeForTitle("G-Earth Dark"); // ThemeFactory.getDefaultTheme()
+        DefaultTitleBarConfig config = new DefaultTitleBarConfig(stage, defaultTheme) {
+            public boolean displayThemePicker() {
+                return false; // For show bar themes (I like to be redundant)
+            }
+        };
+
+        TitleBarController.create(stage, config); // Idk implementation, but applies the theme to the bar
+        Platform.runLater(() -> {
+            stage.getScene().getRoot().getStyleClass().add(defaultTheme.title().replace(" ", "-").toLowerCase());
+            stage.getScene().getRoot().getStyleClass().add(defaultTheme.isDark() ? "g-dark" : "g-light");
+        });
 
         return loader.getController();
     }
