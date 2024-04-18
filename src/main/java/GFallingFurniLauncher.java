2a0b911b2b5bde11a45a86e9f67f8c29c93a5af5
Julianty123 <julian.a.p.c@hotmail.com>
2024-04-18 01:25:19 -0500

    my own .css style 🤩😎😋😁

--- src/main/java/GFallingFurniLauncher.java
+++ src/main/java/GFallingFurniLauncher.java
@@ -22,18 +22,18 @@ public class GFallingFurniLauncher extends ExtensionFormCreator {
         stage.setResizable(false);
         stage.setAlwaysOnTop(true);
 
-        Theme defaultTheme = ThemeFactory.themeForTitle("G-Earth Dark"); // ThemeFactory.getDefaultTheme()
-        DefaultTitleBarConfig config = new DefaultTitleBarConfig(stage, defaultTheme) {
-            public boolean displayThemePicker() {
-                return false; // For show bar themes (I like to be redundant)
-            }
-        };
-
-        TitleBarController.create(stage, config); // Idk implementation, but applies the theme to the bar
-        Platform.runLater(() -> {
-            stage.getScene().getRoot().getStyleClass().add(defaultTheme.title().replace(" ", "-").toLowerCase());
-            stage.getScene().getRoot().getStyleClass().add(defaultTheme.isDark() ? "g-dark" : "g-light");
-        });
+//        Theme defaultTheme = ThemeFactory.themeForTitle("G-Earth Dark"); // ThemeFactory.getDefaultTheme()
+//        DefaultTitleBarConfig config = new DefaultTitleBarConfig(stage, defaultTheme) {
+//            public boolean displayThemePicker() {
+//                return false; // For show bar themes (I like to be redundant)
+//            }
+//        };
+//
+//        TitleBarController.create(stage, config); // Idk implementation, but applies the theme to the bar
+//        Platform.runLater(() -> {
+//            stage.getScene().getRoot().getStyleClass().add(defaultTheme.title().replace(" ", "-").toLowerCase());
+//            stage.getScene().getRoot().getStyleClass().add(defaultTheme.isDark() ? "g-dark" : "g-light");
+//        });
 
         return loader.getController();
     }
